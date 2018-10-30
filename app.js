3fc54d25686d79b9d3f765d9914e4cc820b5db51
Doug DeJong <dougdejong@gmail.com>
2018-10-30 14:30:26 -0400

    added mlab and mutations

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-const express = require('express');
-const graphqlHTTP =  require('express-graphql');
-const schema = require('./schema/schema')
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
 
-app.use('/graphql', graphqlHTTP({
-  schema,
-  graphiql: true
-}));
-
-app.listen(4000,()=> {
-  console.log('listening on port 4000')
+// mongoose setup
+mongoose.connect('mongodb://dej:test123@ds147003.mlab.com:47003/dex-app-gql');
+mongoose.connection.once('open', () => {
+  console.log('connected to M-Lab database');
 })
+
+
+// graphql setup
+
+app.use("/graphql",graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+
+
+// port setup
+
+app.listen(4000, () => {
+  console.log("listening on port 4000");
+});
--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,8 @@
 const graphql = require('graphql');
 const {GraphQLObjectType, GraphQLString, GraphQLSchema, GraphQLID, GraphQLInt, GraphQLList} = graphql;
 const _ = require('lodash');
+const Book = require('../models/book');
+const Author = require('../models/author');
 
 
 // seed data for books 
@@ -44,7 +46,7 @@ const AuthorType = new GraphQLObjectType({
       books: {
         type: new GraphQLList(BookType),
         resolve(parent, args){
-          return _.filter(books, {authorId: parent.id })
+          //return _.filter(books, {authorId: parent.id })
         }
       }
   })
@@ -66,18 +68,44 @@ const RootQuery = new GraphQLObjectType({
       args: {id:{type: GraphQLID}},
       resolve(parent, args){
         
-        return _.find(authors, {id: args.id})
+        //return _.find(authors, {id: args.id})
       }
 
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args){
-        return books
+        //return books
+      }
+    },
+    authors: {
+      type: new GraphQLList(AuthorType),
+      resolve(parent, args){
+        //return authors
       }
     }
   }
 });
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields:{
+    addAuthor: {
+      type: AuthorType,
+      args: {
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
+      },
+      resolve(parent, args) {
+        let author = new Author({
+          name: args.name,
+          age: args.age
+        })
+        author.save();
+      }
+    }
+  }
+})
 
 module.exports = new GraphQLSchema({
   query: RootQuery
