03d68c5cb3493aeba865c605bf99e6e169f47356
Doug DeJong <dougdejong@gmail.com>
2018-10-30 16:55:18 -0400

    added front end functionality

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 
 
 const app = express();
+
+
+app.use(cors())
 
 
 // mongoose setup
